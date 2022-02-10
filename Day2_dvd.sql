7e88ede460a325bc804605c45bc00a6a5bf35876
jnance1979 <jnance1979@gmail.com>
2022-02-09 20:30:24 -0600

    first commit

---
+++ Day2_dvd.sql
@@ -0,0 +1,92 @@
+
+-- ####  #1  ####
+
+select address.address_id, district, first_name, last_name
+from address
+join customer on address.address_id = customer.address_id
+where district = 'Texas'
+group by district, address.address_id, first_name, last_name
+
+-- ####  #2  ####
+
+select payment.customer_id, first_name, last_name, amount
+from payment
+join customer on payment.customer_id = customer.customer_id 
+where amount > 6.99
+group by payment.customer_id, first_name, last_name, amount
+
+-- ####  #3  ####
+--select customer_id, sub.totes from
+--(select customer_id, sum(amount) as totes
+--from payment
+--group by customer_id) sub           returns amounts, but no names (before join)
+--where totes > 175
+--group by customer_id, sub.totes
+
+select customer_id, first_name, last_name, sub.totes from
+(select payment.customer_id, first_name, last_name, sum(amount) as totes
+from payment
+join customer on payment.customer_id = customer.customer_id
+group by payment.customer_id, first_name,last_name) sub
+where totes > 175
+group by customer_id, sub.totes, first_name, last_name
+
+-- ####  #4  ####
+
+ select address.address_id, first_name, last_name, address.city_id, country_id
+from address
+inner join customer on address.address_id = customer.address_id
+inner join city on address.city_id = city.city_id
+where country_id = 66
+group by address.address_id, first_name, last_name, address.city_id, country_id
+
+-- ####  #5  ####
+
+select first_name, last_name, staff.staff_id, count(staff.staff_id) 
+from payment
+join staff on payment.staff_id = staff.staff_id
+group by staff.staff_id
+order by count desc
+limit 1;
+
+-- ####  #6  ####
+select rating, count(film_id)
+from film
+group by rating
+ORDER BY count DESC 
+
+-- ####  #7  ####
+
+select customer_id, amount 
+from payment
+order by customer_id 
+
+select distinct customer_id, first_name, last_name 
+from
+(select payment.customer_id, first_name, last_name, amount
+from payment
+join customer on payment.customer_id = customer.customer_id
+where amount > 6.99) sub
+group by customer_id, first_name, last_name 
+having count(*) = 1
+
+
+--select distinct customer_id
+--from
+--(select customer_id, amount
+--from payment
+--where amount > 6.99) sub
+--group by customer_id
+--having count(*) = 1
+
+-- ####  #8  ####
+
+select count(*)
+from payment
+where amount = 0
+
+
+
+
+
+
